80edf2c3a728adf5945f015ecb608bdf4a43bf46
natalia-se <flaunder1000@gmail.com>
2021-09-22 13:17:29 +0200

    add index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+class Animal {
+  constructor(name, legs) {
+    this.name = name;
+    this.legs = legs;
+  }
+  speech() {
+    return `${this.name} makes a nose`;
+  }
+  numberOfLegs() {
+    return `This animal has ${this.legs} legs`;
+  }
+}
+
+const dog = new Animal("dog", 4);
+console.log(dog.speech());
+console.log(dog.numberOfLegs());
